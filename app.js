5cda6e401da6261a710af448094548ed8621d603
deevanshumahajan <deevanshu.mahajan@coxautoinc.com>
2026-04-21 23:32:15 +0530

    added CI/CD pipeline

---
+++ app.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+// Use AWS provided port or default to 3000
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World from Node.js 🚀");
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
